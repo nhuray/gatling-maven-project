f4f28b75b2aef75f2a87dd6ba2b107b185b61469
Stephane Landelle <slandelle+github@excilys.com>
2012-03-19 17:28:12 +0100

    Remove txt that's no longer supported

---
+++ src/main/resources/simulations/advanced/AdvancedExampleSimulation.scala
@@ -0,0 +1,17 @@
+package advanced
+import com.excilys.ebi.gatling.core.Predef._
+import com.excilys.ebi.gatling.http.Predef._
+
+class AdvancedExampleSimulation extends Simulation {
+
+	def apply = {
+
+		val urlBase = "http://excilys-bank-web.cloudfoundry.com"
+
+		val httpConf = httpConfig.baseURL(urlBase)
+
+		List(
+			SomeScenario.scn.configure.users(10).ramp(10).protocolConfig(httpConf),
+			SomeOtherScenario.otherScn.configure.users(5).ramp(20).delay(30).protocolConfig(httpConf))
+	}
+}
